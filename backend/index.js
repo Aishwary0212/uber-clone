0bee8bee329f2cc766f17912bbfb15f7e081300a
Aishwary <aishwary0212@gmail.com>
2026-04-15 00:28:49 +0530

    created user schema

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import {connectToDB} from "./db/db.js";
 dotenv.config();
 
 
 const app=express();
 app.use(cors());
-
+connectToDB();
 app.get("/",(req,res)=>{
     res.send("hello");
 })
---
+++ backend/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type:String,
+            required: true,
+            minlength:[3, "First name must be at least 3 characters long"]
+        },
+        lastname: {
+            type:String,
+            minlength:[3, "Last name must be at least 3 characters long"]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select:false
+    },
+    socketId: {
+        type: String
+    }
+})
+
+userSchema.methods.generateToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY)
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.statics.hashPassword= async function(password){
+    return await bcrypt.hash(password, 10)
+}
+
+const userModel= mongoose.model("user", userSchema)
+export default userModel
